df56b836e007329785a51a20017284055d7d2dcb
lennart <lennartsteinhoff@gmx.de>
2023-04-30 12:42:43 -0700

    Time Control of main loop (conttroler)

--- src/control_unit/control_main.cpp
+++ src/control_unit/control_main.cpp
@@ -39,15 +39,21 @@ int main() {
 
     bool run(true);
     Message::Data measurement;
+
+    // chrono::time_point start = std::chrono::system_clock::now();
+    constexpr chrono::duration TARGET_FREQ = chrono::milliseconds(1000);
     
 
     while(run) {
+        auto start = std::chrono::system_clock::now();
         input.HandleInput(controller, run);
         measurement = controller->GetMeasurement();
         cout << controller->printStatus() << endl;
         display.VisualizeMeasurement(measurement);
-        
-        std::this_thread::sleep_for(chrono::milliseconds(1000));
+        auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start);
+
+        cout << "Controller, Mainloop duration: " << (duration_ms).count()  << "ms" <<endl;
+        std::this_thread::sleep_for(TARGET_FREQ - duration_ms);
     }
 
     c1.join();
